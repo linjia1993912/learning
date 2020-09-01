223edc1a186bdd2733dc27bbb63ecafcb6d2dc66
差一点成帅哥 <57213312+linjia1993912@users.noreply.github.com>
2020-09-01 16:47:33 +0800

    1.springmvc

---
+++ springmvc/src/com/linjia/controller/MyController.java
@@ -0,0 +1,26 @@
+package com.linjia.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @Description:
+ * @Author LinJia
+ * @Date 2020/9/1
+ **/
+@Controller
+public class MyController {
+
+    /**
+     * @Description:返回值hello，根据前面的配置文件会去/WEB-INF/page/下面找hello.jsp。所以需要新建这个jsp
+     * @Author LinJia
+     * @Date 2020/9/1 16:27
+     * @Param []
+     * @return java.lang.String
+     **/
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ springmvc/web/WEB-INF/page/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: linjia
+  Date: 2020/9/1
+  Time: 16:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello springmvc</title>
+</head>
+<body>
+    <h1>hello springmvc</h1>
+</body>
+</html>
