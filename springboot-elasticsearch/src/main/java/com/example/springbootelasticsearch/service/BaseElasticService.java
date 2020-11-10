59628a2d3071fa4f4d6881d846e15b6ba3b10f35
差一点成帅哥 <57213312+linjia1993912@users.noreply.github.com>
2020-11-10 17:06:05 +0800

    1.集成elasticsearch

---
+++ springboot-elasticsearch/src/main/java/com/example/springbootelasticsearch/service/BaseElasticService.java
@@ -0,0 +1,216 @@
+package com.example.springbootelasticsearch.service;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.support.IndicesOptions;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.reindex.DeleteByQueryRequest;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @Description: 核心操作类
+ * @Author LinJia
+ * @Date 2020/11/10
+ **/
+@Slf4j
+@Component
+public class BaseElasticService {
+
+    @Autowired
+    RestHighLevelClient restHighLevelClient;
+
+    /**
+     * @Description:创建索引
+     * @Author LinJia
+     * @Date 2020/11/10 16:11
+     * @Param [idxName 索引名称, idxSQL 索引描述]
+     * @return void
+     **/
+    public void createIndex(String idxName,String idxSQL){
+        try {
+            if (!this.indexExist(idxName)) {
+                log.error(" idxName={} 已经存在,idxSql={}",idxName,idxSQL);
+                return;
+            }
+            CreateIndexRequest request = new CreateIndexRequest(idxName);
+            buildSetting(request);
+            request.mapping(idxSQL, XContentType.JSON);
+            CreateIndexResponse res = restHighLevelClient.indices().create(request, RequestOptions.DEFAULT);
+            if (!res.isAcknowledged()) {
+                throw new RuntimeException("初始化失败");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * @Description:断某个index是否存在
+     * @Author LinJia
+     * @Date 2020/11/10 16:55
+     * @Param [idxName]
+     * @return boolean
+     **/
+    public boolean indexExist(String idxName) throws Exception {
+        GetIndexRequest request = new GetIndexRequest(idxName);
+        request.local(false);
+        request.humanReadable(true);
+        request.includeDefaults(false);
+        request.indicesOptions(IndicesOptions.lenientExpandOpen());
+        return restHighLevelClient.indices().exists(request, RequestOptions.DEFAULT);
+    }
+
+    /**
+     * @Description:断某个index是否存在
+     * @Author LinJia
+     * @Date 2020/11/10 16:56
+     * @Param [idxName]
+     * @return boolean
+     **/
+    public boolean isExistsIndex(String idxName) throws Exception {
+        return restHighLevelClient.indices().exists(new GetIndexRequest(idxName),RequestOptions.DEFAULT);
+    }
+
+    /**
+     * @Description:设置分片
+     * @Author LinJia
+     * @Date 2020/11/10 16:54
+     * @Param [request]
+     * @return void
+     **/
+    public void buildSetting(CreateIndexRequest request){
+        request.settings(Settings.builder().put("index.number_of_shards",3)
+                .put("index.number_of_replicas",2));
+    }
+
+    /**
+     * @Description:插入或更新
+     * @Author LinJia
+     * @Date 2020/11/10 16:56
+     * @Param [idxName, entity]
+     * @return void
+     **/
+    public void insertOrUpdateOne(String idxName, ElasticEntity entity) {
+        IndexRequest request = new IndexRequest(idxName);
+        log.error("Data : id={},entity={}",entity.getId(), JSON.toJSONString(entity.getData()));
+        request.id(entity.getId());
+        request.source(entity.getData(), XContentType.JSON);
+        try {
+            restHighLevelClient.index(request, RequestOptions.DEFAULT);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @Description:批量插入数据
+     * @Author LinJia
+     * @Date 2020/11/10 16:57 
+     * @Param [idxName, list]
+     * @return void
+     **/
+    public void insertBatch(String idxName, List<ElasticEntity> list) {
+        BulkRequest request = new BulkRequest();
+        list.forEach(item -> request.add(new IndexRequest(idxName).id(item.getId())
+                .source(item.getData(), XContentType.JSON)));
+        try {
+            restHighLevelClient.bulk(request, RequestOptions.DEFAULT);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @Description:批量删除
+     * @Author LinJia
+     * @Date 2020/11/10 16:57 
+     * @Param [idxName, idList]
+     * @return void
+     **/
+    public <T> void deleteBatch(String idxName, Collection<T> idList) {
+        BulkRequest request = new BulkRequest();
+        idList.forEach(item -> request.add(new DeleteRequest(idxName, item.toString())));
+        try {
+            restHighLevelClient.bulk(request, RequestOptions.DEFAULT);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @Description:查询
+     * @Author LinJia
+     * @Date 2020/11/10 16:58 
+     * @Param [idxName, builder, c]
+     * @return java.util.List<T>
+     **/
+    public <T> List<T> search(String idxName, SearchSourceBuilder builder, Class<T> c) {
+        SearchRequest request = new SearchRequest(idxName);
+        request.source(builder);
+        try {
+            SearchResponse response = restHighLevelClient.search(request, RequestOptions.DEFAULT);
+            SearchHit[] hits = response.getHits().getHits();
+            List<T> res = new ArrayList<>(hits.length);
+            for (SearchHit hit : hits) {
+                res.add(JSON.parseObject(hit.getSourceAsString(), c));
+            }
+            return res;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @Description:删除index
+     * @Author LinJia
+     * @Date 2020/11/10 16:58
+     * @Param [idxName]
+     * @return void
+     **/
+    public void deleteIndex(String idxName) {
+        try {
+            if (!this.indexExist(idxName)) {
+                log.error(" idxName={} 已经存在",idxName);
+                return;
+            }
+            restHighLevelClient.indices().delete(new DeleteIndexRequest(idxName), RequestOptions.DEFAULT);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteByQuery(String idxName, QueryBuilder builder) {
+        DeleteByQueryRequest request = new DeleteByQueryRequest(idxName);
+        request.setQuery(builder);
+        //设置批量操作数量,最大为10000
+        request.setBatchSize(10000);
+        request.setConflicts("proceed");
+        try {
+            restHighLevelClient.deleteByQuery(request, RequestOptions.DEFAULT);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ springboot-elasticsearch/src/test/java/com/example/springbootelasticsearch/SpringbootElasticsearchApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.example.springbootelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
