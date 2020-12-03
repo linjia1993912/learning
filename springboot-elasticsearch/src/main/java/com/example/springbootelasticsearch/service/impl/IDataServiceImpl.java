ad6e40e79c069cbf4f579dfc9ee5d1da414d6c5e
linjia <linjiait@sina.com>
2020-12-03 16:09:08 +0800

    1.完善数据爬取功能

--- springboot-elasticsearch/src/main/java/com/example/springbootelasticsearch/service/impl/IDataServiceImpl.java
+++ springboot-elasticsearch/src/main/java/com/example/springbootelasticsearch/service/impl/IDataServiceImpl.java
@@ -11,6 +11,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -30,6 +31,11 @@ public class IDataServiceImpl implements DataService {
 
     @Autowired
     private PhoneRepository phoneRepository;
+
+    @Autowired
+    private ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+    private long time = 10 * 1000L;
 
     /**
      * @Description:删除所有文档
@@ -56,7 +62,23 @@ public class IDataServiceImpl implements DataService {
         //100页
         for (int i = 1; i < 100; i++) {
             String url = "https://list.jd.com/list.html?cat=9987%2C653%2C655&page=" + i + "&s=1&click=0";
-            grabJdPhone(url);
+
+            //使用线程池抓取数据
+            int idleThread = threadPoolTaskExecutor.getCorePoolSize() - threadPoolTaskExecutor.getActiveCount();
+            System.out.println("线程空闲数: " + idleThread);
+
+            // 当没有空闲线程时进行等待
+            while (idleThread == 0) {
+                try {
+                    Thread.sleep(time);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                idleThread = threadPoolTaskExecutor.getCorePoolSize() - threadPoolTaskExecutor.getActiveCount();
+            }
+            log.info("抓取第 "+i+" 页");
+            //有空闲线程则开始爬取数据
+            threadPoolTaskExecutor.execute(()-> grabJdPhone(url));
         }
         log.info("数据抓取完成");
     }
@@ -80,7 +102,7 @@ public class IDataServiceImpl implements DataService {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        log.info("【抓取数据】抓取JD手机信息：{}", result);
+        //log.info("【抓取数据】抓取JD手机信息：{}", result);
         //解析DOM
         Document document = Jsoup.parse(result);
         Elements elementsByClass = document.getElementsByClass("gl-item");
@@ -94,11 +116,13 @@ public class IDataServiceImpl implements DataService {
             String img = imgElement.attr("data-lazy-img");
             phoneEntity.setImgUrl(img);
             log.info("图片:"+img);
+
             //名称
             Elements nameElement = element.select(".p-name em");
             String name = nameElement.text();
             phoneEntity.setName(name);
             log.info("手机名称:"+name);
+
             //广告
             Elements adElement = element.select(".p-name i");
             String ad = adElement.text();
@@ -110,8 +134,8 @@ public class IDataServiceImpl implements DataService {
             for (Element attrElement : attrElements) {
                 String attrElementValue = attrElement.text();
                 if (attrElementValue.contains("运存")) {
-                    log.info("内存：" + attrElementValue);
                     phoneEntity.setMemory(attrElementValue);
+                    log.info("内存：" + attrElementValue);
                 }
                 //容量
                 if (attrElementValue.contains("GB") && !attrElementValue.contains("运存")){
@@ -119,8 +143,8 @@ public class IDataServiceImpl implements DataService {
                     log.info("存储：" + attrElementValue);
                 }
                 if (attrElementValue.contains("英寸")) {
-                    log.info("屏幕：" + attrElementValue);
                     phoneEntity.setScreen(attrElementValue);
+                    log.info("屏幕：" + attrElementValue);
                 }
             }
             //获取售价
@@ -128,8 +152,8 @@ public class IDataServiceImpl implements DataService {
             String price = priceElement.text();
             if (StringUtils.isNotEmpty(price)) {
                 try {
-                    log.info("价格:"+price);
                     phoneEntity.setPrice(BigDecimal.valueOf(Double.parseDouble(price)));
+                    log.info("价格:"+price);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -151,6 +175,6 @@ public class IDataServiceImpl implements DataService {
      * @return void
      **/
     private void esSave(PhoneEntity phoneEntity){
-        phoneRepository.save(phoneEntity);
+            phoneRepository.save(phoneEntity);
     }
 }
---
+++ springboot-elasticsearch/src/main/java/com/example/springbootelasticsearch/util/ThreadPoolTaskExecutorConfig.java
@@ -0,0 +1,53 @@
+package com.example.springbootelasticsearch.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+
+/**
+ * @Description:Spring配置线程池
+ * 还可以基于xml配置
+ * @Author LinJia
+ * @Date 2019/10/25
+ **/
+@Configuration
+@EnableAsync
+public class ThreadPoolTaskExecutorConfig {
+
+
+    /**
+     * @Description:
+     * 拒绝策略
+     * rejectedExectutionHandler参数字段用于配置绝策略，常用拒绝策略如下
+     * AbortPolicy：用于被拒绝任务的处理程序，它将抛出RejectedExecutionException
+     * CallerRunsPolicy：用于被拒绝任务的处理程序，它直接在execute方法的调用线程中运行被拒绝的任务。
+     * DiscardOldestPolicy：用于被拒绝任务的处理程序，它放弃最旧的未处理请求，然后重试execute。
+     * DiscardPolicy：用于被拒绝任务的处理程序，默认情况下它将丢弃被拒绝的任务。
+     **/
+    @Bean
+    public ThreadPoolTaskExecutor asyncServiceExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        //配置核心线程数
+        executor.setCorePoolSize(5);
+        //配置最大线程数
+        executor.setMaxPoolSize(5);
+        // 设置线程活跃时间（秒）
+        executor.setKeepAliveSeconds(120);
+        //配置队列大小
+        executor.setQueueCapacity(100);
+        //配置线程池中的线程的名称前缀
+        executor.setThreadNamePrefix("grapData-service-");
+        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
+        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        //执行初始化
+        executor.initialize();
+        System.out.println("线程池初始化完成");
+        return executor;
+    }
+
+}
