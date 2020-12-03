175172a9ca50a44b3029683df8d6e0d7b35a1a0f
linjia <linjiait@sina.com>
2020-12-03 16:18:11 +0800

    1.完善数据爬取功能

--- springboot-elasticsearch/src/main/java/com/example/springbootelasticsearch/util/ThreadPoolTaskExecutorConfig.java
+++ springboot-elasticsearch/src/main/java/com/example/springbootelasticsearch/util/ThreadPoolTaskExecutorConfig.java
@@ -42,7 +42,7 @@ public class ThreadPoolTaskExecutorConfig {
         //配置线程池中的线程的名称前缀
         executor.setThreadNamePrefix("grapData-service-");
         // rejection-policy：当pool已经达到max size的时候，如何处理新任务
-        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
+        // CallerRunsPolicy：不在新线程中执行任务，而是有调用者所在的线程来执行
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         //执行初始化
         executor.initialize();
