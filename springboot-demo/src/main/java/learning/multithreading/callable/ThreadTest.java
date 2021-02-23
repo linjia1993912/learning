36ead83b4da6c6b68c049298e2b01543f2a158f8
linjia <linjiait@sina.com>
2021-02-23 15:16:41 +0800

    1.线程知识

--- springboot-demo/src/main/java/learning/multithreading/callable/ThreadTest.java
+++ springboot-demo/src/main/java/learning/multithreading/callable/ThreadTest.java
@@ -27,7 +27,7 @@ public class ThreadTest {
         System.out.println("主线程for循环执行完毕..");
         try {
 
-            int sum = futureTask.get();
+            int sum = futureTask.get(); //FutureTask.get()会阻塞,直到MyCallable执行完毕才能获取到返回值
 
             System.out.println("sum = " + sum);
         } catch (InterruptedException e) {
@@ -35,6 +35,11 @@ public class ThreadTest {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+
+        /**
+         执行下此程序，我们发现sum = 4950永远都是最后输出的。而“主线程for循环执行完毕..”则很可能是在子线程循环中间输出。由CPU的线程调度机制，我们知道，“主线程for循环执行完毕..”的输出时机是没有任何问题的，那么为什么sum =4950会永远最后输出呢？
+         原因在于通过ft.get()方法获取子线程call()方法的返回值时，当子线程此方法还未执行完毕，ft.get()方法会一直阻塞，直到call()方法执行完毕才能取到返回值。
+         **/
 
     }
 }
