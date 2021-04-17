967710ef523e17010b0de9df9d7515cc7af8afa6
mac <linjiait@sina.com>
2021-04-17 19:41:00 +0800

    1.增加mybatis-plus项目

---
+++ springboot-mybatis-plus/src/test/java/com/example/mybatisplus/SpringbootMybatisPlusApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
